bdad6088755f13f45debca90a4e4c85a8eee115d
lwhb <lawhub.tokyo@gmail.com>
2019-12-29 17:00:32 +0900

    ISSUE40: throw exception when multiple target text found

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from lawhub.action import Action
-from lawhub.apply import apply_replace, TextNotFoundError, apply_add_after, apply_delete
+from lawhub.apply import apply_replace, TextNotFoundError, MultipleTextFoundError, apply_add_after, apply_delete
 from lawhub.law import Paragraph
 from lawhub.query import Query
 
@@ -21,6 +21,14 @@ class TestApply(TestCase):
         action = Action('第一項中「サル」を「イヌ」に改める')
 
         with self.assertRaises(TextNotFoundError):
+            apply_replace(action, query2node)
+
+    def test_apply_replace_multiple_fail(self):
+        node = Paragraph(title='第一項', sentence='私はネコネコです')
+        query2node = {Query('第一項'): node}
+        action = Action('第一項中「ネコ」を「イヌ」に改める')
+
+        with self.assertRaises(MultipleTextFoundError):
             apply_replace(action, query2node)
 
     def test_add_after(self):
