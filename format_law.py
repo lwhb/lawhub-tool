6617b15a0796a1cf3635a710c08634a01c4f9bca
lwhb <lawhub.tokyo@gmail.com>
2020-05-05 21:49:36 +0900

    ISSUE50: fixed format_law.py with updated GianParser

--- format_law.py
+++ format_law.py
@@ -9,28 +9,25 @@ LOGGER = logging.getLogger('format_law')
 
 def main(in_fp, out_fp):
     with open(in_fp, 'r') as f:
-        lines = [line.strip() for line in f]
+        lines = [line.strip() for line in f if line.strip()]
     LOGGER.info(f'Loaded text from {in_fp}')
 
-    gian_parser = GianParser()
-    lines_and_nodes = gian_parser.parse(lines)
+    parser = GianParser()
+    parse_result = parser.parse(lines)
 
     with open(out_fp, 'w') as f:
-        for obj in lines_and_nodes:
-            if isinstance(obj, str):
-                LOGGER.warning(f'failed to convert {obj} to law node')
-                f.write('!!' + obj + '\n')
-            else:
-                f.write(str(obj) + '\n')
+        for entry in parse_result:
+            for node in entry.nodes:
+                f.write(f'{node}\n')
     LOGGER.info(f'Saved formatted text to {out_fp}')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='生TEXTの法律文を受け取り整形して出力する')
-    parser.add_argument('-i', '--input', default='./data/format_law.in')
-    parser.add_argument('-o', '--output', default='./data/format_law.out')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    args = parser.parse_args()
+    argparser = argparse.ArgumentParser(description='生TEXTの法律文を受け取り整形して出力する')
+    argparser.add_argument('-i', '--input', default='./data/format_law.in')
+    argparser.add_argument('-o', '--output', default='./data/format_law.out')
+    argparser.add_argument('-v', '--verbose', action='store_true')
+    args = argparser.parse_args()
 
     logging.basicConfig(
         level=logging.DEBUG if args.verbose else logging.INFO,
